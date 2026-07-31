5d94c4dd6252b21c7564e402a320a2656058e064
Gousiya parveen Syed <gousiyaparveen2006@gmail.com>
2026-08-01 00:35:47 +0530

    Added review service

--- frontend/src/services/reviewservice.js
+++ frontend/src/services/reviewservice.js
@@ -1,11 +1,25 @@
 import API from "./api";
 
+// Get reviews
 export const getReviews = async (productId) => {
   const { data } = await API.get(`/reviews/${productId}`);
   return data.reviews;
 };
 
+// Add review
 export const addReview = async (reviewData) => {
   const { data } = await API.post("/reviews", reviewData);
+  return data;
+};
+
+// Update review
+export const updateReview = async (id, reviewData) => {
+  const { data } = await API.put(`/reviews/${id}`, reviewData);
+  return data;
+};
+
+// Delete review
+export const deleteReview = async (id) => {
+  const { data } = await API.delete(`/reviews/${id}`);
   return data;
 };
